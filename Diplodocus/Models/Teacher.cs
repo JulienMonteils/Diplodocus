c41c3ba8b17264cbff52c3624f99819958b44997
vincent pruvost <v.pruvost91@gmail.com>
2018-04-16 13:56:44 +0200

    sgfzeg

---
+++ Diplodocus/Models/Teacher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Diplodocus.Models
+{
+    public class Teacher : User
+    {
+
+        public Teacher(string firstName, string lastName, string address, string phoneNumber)
+            : base("", "", "", "")
+        // appel du constructeur de la classe CompteBancaire
+        // le mot-clé "base" permet d'accéder à la classe parente
+        {
+        }
+
+        protected override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ Diplodocus/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Diplodocus.Models
+{
+    public abstract class User
+    {
+        // "m_" == données membres
+        protected string m_firstName { get; set; }
+        protected string m_lastName { get; set; }
+        protected string m_address { get; set; }
+        protected string m_phoneNumber { get; set; }
+
+        protected User(string firstName, string lastName, string address, string phoneNumber)
+        {
+            this.m_firstName = firstName;
+            this.m_firstName = lastName;
+            this.m_address = firstName;
+            this.m_phoneNumber = lastName;
+        }
+
+        protected abstract string ToString();
+
+    }
+}
