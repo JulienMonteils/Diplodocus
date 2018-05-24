0edf79d88199ae7321742b9280d2d2a7fd690e7d
Ewenfayat <ewenfayat@gmail.com>
2018-05-24 09:40:03 +0200

    ok ewen

---
+++ Diplodocus/Controllers/ViewStudentController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Diplodocus.Controllers
+{
+    public class ViewStudentController : Controller
+    {
+        // GET: ViewStudent
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewAccueilEtudiant()
+        {
+            return View();
+        }
+    }
+}
