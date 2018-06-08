2d363d0ac7bd1b7d9fb17bd0b4dd75c36c11facc
JulienMonteils <monteils.julien@gmail.com>
2018-06-08 22:54:37 +0200

    acces au differentes fonctionnalité du manager par un menu

---
+++ Diplodocus/Controllers/StartController.cs
@@ -0,0 +1,63 @@
+﻿using Diplodocus.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Diplodocus.Controllers
+{
+    public class StartController : Controller
+    {
+        private MyContext db = new MyContext();
+
+        // GET: Grades
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult ManagerStart()
+        {
+            return View();
+
+           
+        }
+
+
+        public ActionResult StudentStart()
+        {
+            return View();
+
+           
+        }
+
+
+        public ActionResult ManageStudentRedirect()
+        {
+
+
+            return RedirectToAction("Index", "Students");
+        }
+
+
+        public ActionResult ManageGradeRedirect()
+        {
+
+
+            return RedirectToAction("Index", "Grades");
+        }
+
+        public ActionResult ManageSubjectRedirect()
+        {
+
+
+            return RedirectToAction("Index", "SchoolSubjects");
+        }
+
+
+    }
+}
